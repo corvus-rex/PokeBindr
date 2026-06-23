e7c3837775cb0ec53143f457945cfc39e74f3c88
Adrian Riadi <adrianovriadi@gmail.com>
2026-06-23 15:53:27 +0700

    Scaffold Vue 3 frontend with landing page

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './views/LandingPage.vue'
+</script>
+
+<template>
+  <LandingPage />
+</template>
---
+++ frontend/src/views/LandingPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="landing">
+    <h1>PokéBindr</h1>
+    <p>Share your digital Pokémon card binder.</p>
+  </main>
+</template>
+
+<style scoped>
+.landing {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+  font-family: sans-serif;
+}
+</style>
